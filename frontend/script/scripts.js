efa3750e193c75139017addbe8438988ba9aece5
SamahShahla0 <samahch6@gmail.com>
2024-04-27 09:58:21 +0300

    frontend <> implemented and styled catalog page with load more button

--- frontend/script/scripts.js
+++ frontend/script/scripts.js
@@ -35,6 +35,9 @@ else if(pPath == "sign-in-up.html"){
 else if(pPath == "cart.html"){
   cartPage()
 }
+else if(pPath == "catalog.html"){
+  catalogPage()
+}
 
 
 // Define functions for each page
@@ -67,4 +70,156 @@ function cartPage() {
     $(this).parent().parent().parent().hide( 400 );
   
   })
+}
+
+
+function catalogPage() {
+  const cardContainer = document.getElementById("card-container");
+  const loadMoreButton = document.getElementById("load-more");
+
+
+  const cardLimit = 10;
+  const cardIncrease = 5;
+  const pageCount = Math.ceil(cardLimit / cardIncrease);
+  let currentPage = 1;
+
+  const handleButtonStatus = () => {
+    if (pageCount === currentPage) {
+      loadMoreButton.classList.add("disabled");
+      loadMoreButton.setAttribute("disabled", true);
+    }
+  };
+
+  const createBookCard = (bookData) => {
+    const card = document.createElement("div");
+    card.className = "book-cell";
+  
+    const bookImgDiv = document.createElement("div");
+    bookImgDiv.className = "book-img";
+    const bookImg = document.createElement("img");
+    bookImg.className = "book-photo";
+    bookImg.src = bookData.imgSrc;
+    bookImg.alt = bookData.title;
+    bookImgDiv.appendChild(bookImg);
+  
+    const bookContentDiv = document.createElement("div");
+    bookContentDiv.className = "book-content";
+    const bookTitleDiv = document.createElement("div");
+    bookTitleDiv.className = "book-title";
+    bookTitleDiv.textContent = bookData.title;
+    const bookAuthorDiv = document.createElement("div");
+    bookAuthorDiv.className = "book-author";
+    bookAuthorDiv.textContent = "by " + bookData.author;
+    const bookSumDiv = document.createElement("div");
+    bookSumDiv.className = "book-sum";
+    const insSpan = document.createElement("ins");
+    const amountSpan = document.createElement("span");
+    amountSpan.className = "amount";
+    amountSpan.textContent = bookData.price;
+    insSpan.appendChild(amountSpan);
+    bookSumDiv.appendChild(insSpan);
+    
+    const addToCartBtn = document.createElement("button");
+    addToCartBtn.className = "btn";
+    addToCartBtn.textContent = "Add To Cart";
+  
+    bookContentDiv.appendChild(bookTitleDiv);
+    bookContentDiv.appendChild(bookAuthorDiv);
+    bookContentDiv.appendChild(bookSumDiv);
+    bookContentDiv.appendChild(addToCartBtn);
+  
+    card.appendChild(bookImgDiv);
+    card.appendChild(bookContentDiv);
+  
+    cardContainer.appendChild(card);
+  };
+
+  const addCards = (pageIndex) => {
+    currentPage = pageIndex;
+
+    handleButtonStatus();
+
+    const startRange = (pageIndex - 1) * cardIncrease;
+    const endRange =
+      pageIndex * cardIncrease > cardLimit ? cardLimit : pageIndex * cardIncrease;
+
+    // Assuming you have an array of bookData called bookDataList
+    for (let i = startRange; i < endRange && i < bookDataList.length; i++) {
+      createBookCard(bookDataList[i]);
+    }
+  };
+
+  // Example usage:
+  const bookDataList = [
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "http://127.0.0.1:5500/images/harrypotter2.jpg"
+    },
+    // Add more book data objects here...
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "http://127.0.0.1:5500/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "http://127.0.0.1:5500/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "http://127.0.0.1:5500/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "/images/harrypotter2.jpg"
+    },
+    {
+      title: "The ultimate guide to Computer Science",
+      author: "Elizabeth Gilbert",
+      price: "€32,50",
+      imgSrc: "/images/harrypotter2.jpg"
+    }
+  ];
+
+
+  window.onload = function () {
+    addCards(currentPage);
+    loadMoreButton.addEventListener("click", () => {
+      addCards(currentPage + 1);
+    });
+  };
+
 }
